af7ac5cb9433c90ff1957d27df7329aec524ba81
SamuelVins <samuel.vins87@gmail.com>
2026-07-07 11:42:12 +0200

    user can add its Incomes and expenses and see total balance

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button, Alert, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert, TouchableOpacity, ActivityIndicator, Modal } from 'react-native';
 import { supabase } from './supabase';
 
 export default function App() {
@@ -8,6 +8,16 @@ export default function App() {
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState('');
   const [session, setSession] = useState(null); //jestli je nekdo prihlaseny
+
+  // Stavy pro finance
+  const [transactions, setTransactions] = useState([]); //vsechny zaznamy
+  const [balance, setBalance] = useState(0); // zustatek
+  const [modalVisible, setModalVisible] = useState(false); // vyskakovaci okno na pridani zaznamu
+
+  // Stavy pro novy zaznam
+  const [txTitle, setTxTitle] = useState('');
+  const [txAmount, setTxAmount] = useState('');
+  const [txType, setTxType] = useState('income'); // income/expoence (incomo jako default)
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
@@ -21,6 +31,63 @@ export default function App() {
     return () => subscription.unsubscribe();
   }, []);
 
+  async function getTransactions() {
+    if(!session) return;
+
+    const { data, error } = await supabase
+      .from('transactions')
+      .select('*')
+      .eq('user_id', session.user.id) //nacte pouze data uzivatele, ktery je prihlasen
+      .order('created_at', {ascending: false});
+
+      if (error) {
+      console.log('Error getting data:', error.message);
+      } else {
+        setTransactions(data);
+
+        // spocitani zustatku
+        let totalbalance = 0;
+        data.forEach(item => {
+          if (item.type == 'income') totalbalance += Number(item.amount);
+          if (item.type == 'expense') totalbalance -= Number(item.amount);
+        });
+        setBalance(totalbalance);
+      }
+  }
+  
+  //okamzite nacteni dat po prihlaseni uzivatele
+  useEffect(() => {
+    if (session) {
+      getTransactions();
+    }
+  }, [session]);
+
+  async function handleAddTransaction() {
+    if (!txTitle || !txAmount) {
+      Alert.alert("Error", "Please fill in both the name and the amount..");
+      return;
+    }
+
+    const { error } = await supabase
+      .from('transactions')
+      .insert([
+        { 
+          title: txTitle, 
+          amount: parseFloat(txAmount), 
+          type: txType,
+          user_id: session.user.id // propojeni s prihlasenym uzivatelem
+        }
+      ]);
+
+    if (error) {
+      Alert.alert("Error while saving", error.message);
+    } else {
+      setTxTitle('');
+      setTxAmount('');
+      setModalVisible(false);
+      getTransactions(); //prepocteni zustatku
+    }
+  }
 
 
   async function handleLogin() {
@@ -71,17 +138,73 @@ export default function App() {
     await supabase.auth.signOut();
   }
 
+
+  // ***** Pages *****
+  // --- Main page ---
+
   if (session) {
     return(
       <View style={styles.container}>
         <Text>Main page</Text>
         <Text>Welcome {session.user.email}</Text>
+        <Text>Balance:</Text>
+        <Text>{balance} Kč</Text>
+        <TouchableOpacity onPress={() => setModalVisible(true)}>
+          <Text >+ Add record</Text>
+        </TouchableOpacity>
+        <Modal visible={modalVisible} animationType="slide" transparent={false}>
+          <View style={styles.container}>
+            <Text>New record</Text>
+            <Text>Name:</Text>
+            <TextInput
+              placeholder="E.g. Salary, Groceries..."
+              value={txTitle} 
+              onChangeText={setTxTitle}
+            />
+
+            <Text>Amount:</Text>
+            <TextInput 
+              placeholder="0" 
+              value={txAmount} 
+              onChangeText={setTxAmount}
+              keyboardType="numeric"
+            />
+
+            <Text>Transaction type:</Text>
+            <View>
+              <TouchableOpacity 
+                onPress={() => setTxType('income')}
+              >
+                <Text>Income</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity 
+                onPress={() => setTxType('expense')}
+              >
+                <Text>expense</Text>
+              </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity onPress={handleAddTransaction}>
+              <Text>Save record</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => setModalVisible(false)}>
+              <Text>Cancel</Text>
+            </TouchableOpacity>
+
+          </View>
+        </Modal>
+
+
         <TouchableOpacity onPress={handleSignOut}>
           <Text>SignOut</Text>
         </TouchableOpacity>
       </View>
     )
   }
+
+  // -- Login page ---
 
   return (
     <View style={styles.container}>
